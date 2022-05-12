31481b89b044ed453f4879f81e3f92e4aec46bdb
Maria Volkova <safety-team.ru@yandex.ru>
2022-05-12 10:55:22 +0700

    feat: add class Api UserInfo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,21 @@
 import '../pages/index.css';
 import 'core-js/es/symbol';
 
+import UserInfo from '../components/UserInfo';
+import { api } from "../components/Api";
+//===================================================================
+let user
+const userInfo = new UserInfo({
+  nameInput: '.profile__title',
+  aboutInput: '.profile__subtitle',
+  avatarLink: '.profile__avatar'
+});
+
+Promise.all([api.getInfoProfile(), api.getInitialCards()])
+.then(([userData, cards]) => {
+  user = userData;
+  userInfo.addUserInfo(user);
+})
 
 
 
